a7238f92ffbb599ec1aa20086685d53c3a5de17a
grvxs <gauravsinghsisodia5@gmail.com>
2021-06-27 21:46:11 +0530

    Format imports and remove last newline

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import qualified System.Directory as D
+import qualified Control.Monad      as M
+import qualified Data.Char          as C
+import qualified Data.List          as L
+import qualified Data.Maybe         as A
+import qualified System.Directory   as D
 import qualified System.Environment as E
-import qualified System.IO as I
-import qualified Data.List as L
-import qualified Data.Char as C
-import qualified Control.Monad as M
-import qualified Text.Read as R
-import qualified Data.Maybe as A
+import qualified System.IO          as I
+import qualified Text.Read          as R
 
 main :: IO ()
 main = do
